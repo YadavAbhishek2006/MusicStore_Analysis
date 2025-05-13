a5d550959eb6f2ff7d1402208cb5c60606bd1673
YadavAbhishek2006 <abhishekkyadavvvvv@gmail.com>
2025-05-13 22:33:20 +0530

    Update Music_Store_Analysis.sql

--- Music_Store_Analysis.sql
+++ Music_Store_Analysis.sql
@@ -1,4 +1,4 @@
-### /* Q1: Write a query that determines the customer that has spent the most on music for each country. 
+ /* Q1: Write a query that determines the customer that has spent the most on music for each country. 
 Write a query that returns the country along with the top customer and how much they spent. 
 For countries where the top amount spent is shared, provide all customers who spent this amount. */
 
